1a31d70ffdf16f675de13778b606f95c2405d94c
nand-n <nahomdebele002@gmail.com>
2022-11-01 02:37:10 -0400

    progressing well

---
+++ src/handlers/user.ts
@@ -0,0 +1,36 @@
+import prisma from "../db";
+import { comparePassword, createJwt, hashPassword } from "../modules/auth";
+
+export const createNewUser = async (req,res) =>{
+    const user =await prisma.user.create(
+        {
+            data:{
+                username:req.body.username,
+                password:await hashPassword(req.body.password)
+            }
+        }
+    )
+    const token = createJwt(user)
+    if(!token){
+        res.status(401)
+        res.json({message:'Error generating the token'})
+    }
+    res.json({ token })
+}
+
+export const signin =async(req,res)=>{
+    const user = await prisma.user.findUnique({
+        where:{
+            username:req.body.username
+        }
+    })
+
+    const isValid =await comparePassword(req.body.password,user.password)
+    if(!isValid){
+        res.status(401)
+        res.json({message:'nope'})
+        return 
+    }
+    const token =createJwt(user)
+    res.json({token})
+}
--- src/router.ts
+++ src/router.ts
@@ -1,56 +1,73 @@
 import {Router} from 'express';
+import { body,oneOf,validationResult } from 'express-validator';
+import prisma from './db';
+import { createProduct, getOneProduct, getProducts } from './handlers/product';
+import { handleInputErrors } from './modules/middleware';
 
 const router = Router();
 //Product 
-router.get('/product',(req,res) =>{
-    // res.send(200)
-    res.json({message: "message"})
-})
-router.get('/product:id',()=>{
+router.get('/product',getProducts)
+
+router.get('/product/:id',getOneProduct)
+// router.put('/product:id', body('name').isString() ,handleInputErrors)
+
+
+router.post('/product',body('name').isString(),handleInputErrors,createProduct)
+router.post('/product/:id',(req,res)=>{
 
 })
-router.put('/product:id',()=>{
-
-})
-router.post('/product:id',()=>{
-
-})
-router.delete('/product:id',()=>{
+router.delete('/product/:id',()=>{
 
 })
 
 //Update 
-router.get('/update',(req,res) =>{
+// router.get('/update',(req,res) =>{
 
-})
-router.get('/update:id',()=>{
+// })
+// router.get('/update:id',
+// body('title').optional(),
+// body('body').optional(),
+// body('status').isIn(['IN_PROGRESS','SHIPED','DEPERCATED']),
+// body('version').optional(),
+//     ()=>{
 
-})
-router.put('/update:id',()=>{
+// })
 
-})
-router.post('/update',()=>{
+// router.post('/update',
+// body('title').exists().isString(),
+// body('body').exists().isString(),
+// ()=>{
 
-})
-router.delete('/update:id',()=>{
+// })
+// router.put('/update:id',()=>{
 
-})
+// })
+// router.delete('/update:id',()=>{
 
-//Update Point
-router.get('/updatepoint',(req,res) =>{
+// })
 
-})
-router.get('/updatepoint:id',()=>{
+// //Update Point
+// router.get('/updatepoint',(req,res) =>{
 
-})
-router.put('/updatepoint:id',()=>{
+// })
+// router.get('/updatepoint:id',()=>{
 
-})
-router.post('/updatepoint',()=>{
+// })
+// router.post('/updatepoint',
+// body('name').isString(),
+// body('description').isString(),
+// body('updateId').exists().toString(),
+//     ()=>{
 
-})
-router.delete('/updatepoint:id',()=>{
+// })
+// router.put('/updatepoint:id',
+// body('name').optional().isString(),
+// body('description').optional().isString(),
+//     ()=>{
 
-})
+// })
+// router.delete('/updatepoint:id',()=>{
+
+// })
 
 export default router;
